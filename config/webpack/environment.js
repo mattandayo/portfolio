62fc795a8415f4924cd1d8210829d8ce7f031431
mattan <mattandayo@gmail.com>
2018-06-12 14:10:20 +0900

    add vue compliler alias

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,12 @@ const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+environment.toWebpackConfig().merge({
+  resolve: {
+    alias: {
+      '@vue': 'vue/dist/vue.esm.js'
+    }
+  }
+});
+
 module.exports = environment
